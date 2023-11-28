5277ed56377a40731e9fef77f587c18258786cb2
João Bispo <142504691+jotaBMS@users.noreply.github.com>
2023-11-27 23:01:57 -0300

    Update bd.h

--- bd.h
+++ bd.h
@@ -24,16 +24,16 @@ struct s_tema
 
 // Perguntas em forma de constantes, desta maneira as variáveis não têm armazenamento local.
 static const char *perguntas[] = {
-    "\nde uma nota de 1 a 5 para o tema da exposicao que acabou de ver sendo 1 ruim e 5 muito boa",
-    "\nresponda com sim ou nao se voce recomendaria essa exposicao para algum amigo",
-    "\nresponda com sim ou nao se voce retornaria para ver os outros temas de nossa exposicao",//linha de baixo: add temas
-    "\ncaso ja tenha visto nossas outras exposicoes qual voce mais gostou 1 2 3 ou 4? caso nao tenha visto outras responda 5",
-    "\nde uma nota de 1 a 5 para a qualidade de nosso servico",
-    "\nvoce veio com sua familia (1) ou com seus amigos(2)?",
-    "\nresponda com sim ou nao se voce achou o preco do ingresso justo",
-    "\nvoce ja conhecia o tema da exposicao(1) ou foi seu primeiro contato(2) com esse assunto",
-    "\nvoce a estudante(1) pesquisador(2) ou visitante casual(3)",
-    "\nresponda com sim ou nao se voce e maior de idade?",
+    "de uma nota de 1 a 5 para o tema da exposicao que acabou de ver sendo 1 ruim e 5 muito boa",
+    "responda com sim ou nao se voce recomendaria essa exposicao para algum amigo",
+    "responda com sim ou nao se voce retornaria para ver os outros temas de nossa exposicao",//linha de baixo: add temas
+    "caso ja tenha visto nossas outras exposicoes qual voce mais gostou 1 2 3 ou 4? caso nao tenha visto outras responda 5",
+    "de uma nota de 1 a 5 para a qualidade de nosso servico",
+    "voce veio com sua familia (1) ou com seus amigos(2)?",
+    "responda com sim ou nao se voce achou o preco do ingresso justo",
+    "voce ja conhecia o tema da exposicao(1) ou foi seu primeiro contato(2) com esse assunto",
+    "voce a estudante(1) pesquisador(2) ou visitante casual(3)",
+    "responda com sim ou nao se voce e maior de idade?",
     NULL
 };
 
